9215da0057db221ef94c6e0261f613df8b43f3e6
Per-Erik Brorsson <pebr1204@student.miun.se>
2025-06-15 01:04:48 +0200

    start of fixing for push

--- src/main/java/se/sundsvall/teamssender/api/TeamsResource.java
+++ src/main/java/se/sundsvall/teamssender/api/TeamsResource.java
@@ -1,14 +1,28 @@
 package se.sundsvall.teamssender.api;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.Tag;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TeamsResource {
+@Tag(name = "Teams resource", description = "Resource for sending message in Teams")
+@Validated
+class TeamsResource {
 
-	@PostMapping(path = "send/teams/message")
-	public ResponseEntity<String> sendTeamsMessage() {
+	@PostMapping("{municipalityId}/teams/message")
+
+	@Operation(summary = "Send a message in Teams", responses = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Successful Operation",
+					useReturnTypeSchema = true),
+	})
+
+	ResponseEntity<String> sendTeamsMessage() {
 		return ResponseEntity.ok("Sent Successfully");
 	}
 }
