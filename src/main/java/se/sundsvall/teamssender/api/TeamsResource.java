7bf67cfba515dc14ed6e5050e2654c8790ab6372
BrorssonP-E <per-erik.brorsson@sundsvall.se>
2025-06-17 10:42:25 +0200

    fixed formating TeamsResource.java

--- src/main/java/se/sundsvall/teamssender/api/TeamsResource.java
+++ src/main/java/se/sundsvall/teamssender/api/TeamsResource.java
@@ -1,19 +1,21 @@
 package se.sundsvall.teamssender.api;
 
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @Tag(name = "Teams resource", description = "Resource for sending message in Teams")
 @Validated
 class TeamsResource {
 
-	@PostMapping("{municipalityId}/teams/message")
+	@PostMapping("{municipalityId}/teams/messages")
 
 	@Operation(summary = "Send a message in Teams", responses = {
 			@ApiResponse(
