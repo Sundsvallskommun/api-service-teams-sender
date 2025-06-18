959a9f7452f3e7f2783ec7a0d548d13bc8c1c6d8
Per-Erik Brorsson <pebr1204@student.miun.se>
2025-06-18 11:29:34 +0200

    WIP

---
+++ src/main/java/se/sundsvall/teamssender/api/service/TeamsSender.java
@@ -0,0 +1,13 @@
+package se.sundsvall.teamssender.api.service;
+
+import se.sundsvall.teamssender.api.model.SendTeamsMessageRequest;
+
+public interface TeamsSender {
+
+	void sendTeamsMessage(SendTeamsMessageRequest request);
+
+	String getMunicipalityId();
+
+	void setMunicipalityId(String municipalityId);
+
+}
