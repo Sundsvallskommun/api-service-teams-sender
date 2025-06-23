0f5f3989d9a974f8256764262193275f36ce0009
Per-Erik Brorsson <pebr1204@student.miun.se>
2025-06-23 09:02:35 +0200

    Updaterat STMR med det som inte ska vara blankt

--- src/main/java/se/sundsvall/teamssender/api/model/SendTeamsMessageRequest.java
+++ src/main/java/se/sundsvall/teamssender/api/model/SendTeamsMessageRequest.java
@@ -1,6 +1,7 @@
 package se.sundsvall.teamssender.api.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -9,15 +10,18 @@ import lombok.Data;
 
 public class SendTeamsMessageRequest {
 
+	@NotBlank
 	@Schema(description = "The user ID or email to send the message to", example = "first.last@municipaladress.com", required = true)
 	private String user;
 
+	@NotBlank
 	@Schema(description = "The user ID or email to send the message to", example = "first.last@municipaladress.com", required = true)
 	private String sender;
 
 	@Schema(description = "The title of the message", example = "Weekly Update", required = true)
 	private String title;
 
+	@NotBlank
 	@Schema(description = "The content of the message", example = "Don't forget the meeting at 3 PM", required = true)
 	private String message;
 
