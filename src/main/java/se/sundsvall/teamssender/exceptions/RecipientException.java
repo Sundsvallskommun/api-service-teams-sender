2cdf526fee33f506cf46bd929c180ffa2ee63d6f
Per-Erik Brorsson <pebr1204@student.miun.se>
2025-06-24 09:26:57 +0200

    Moved around, added exception

---
+++ src/main/java/se/sundsvall/teamssender/exceptions/RecipientException.java
@@ -0,0 +1,7 @@
+package se.sundsvall.teamssender.exceptions;
+
+public class RecipientException extends RuntimeException {
+    public RecipientException(String message) {
+        super(message);
+    }
+}
