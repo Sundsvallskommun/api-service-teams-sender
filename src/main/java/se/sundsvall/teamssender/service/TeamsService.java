03e11b09806c487c68a59d5c6261b6bc91304bac
BrorssonP-E <per-erik.brorsson@sundsvall.se>
2025-07-09 13:18:07 +0200

    fixed jwt printout

--- src/main/java/se/sundsvall/teamssender/service/TeamsService.java
+++ src/main/java/se/sundsvall/teamssender/service/TeamsService.java
@@ -13,43 +13,7 @@ import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
 
-//package se.sundsvall.teamssender.service;
-//
-//import static java.util.Objects.isNull;
-//import static java.util.stream.Collectors.toMap;
-//
-//import jakarta.validation.Valid;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.function.Function;
-//import lombok.Data;
-//import org.springframework.stereotype.Service;
-//import org.zalando.problem.Problem;
-//import org.zalando.problem.Status;
-//
-//@Service
-//@Data
-//
-//public class TeamsService {
-//
-//	private final Map<String, TeamsSender> chatSenders;
-//
-//	public TeamsService(final List<TeamsSender> chatSenders) {
-//		this.chatSenders = chatSenders.stream()
-//			.collect(toMap(TeamsSender::getMunicipalityId, Function.identity()));
-//	}
-//
-//	public void sendTeamsMessage(final String municipalityId, final se.sundsvall.teamssender.api.model.@Valid SendTeamsMessageRequest request) {
-//		var chatSender = chatSenders.get(municipalityId);
-//		if (isNull(chatSender)) {
-//			throw Problem.valueOf(Status.BAD_GATEWAY, "No mail sender exists for municipalityId " + municipalityId);
-//		}
-//
-//		chatSender.sendTeamsMessage(request);
-//	}
-//}
 @Service
 public class TeamsService {
 
