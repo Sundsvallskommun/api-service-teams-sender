4ca82d997ce1267495faba31f3bf5baa0d32a177
Per-Erik Brorsson <pebr1204@student.miun.se>
2025-07-14 14:03:33 +0200

    Edited MSgraph access token

--- src/main/java/se/sundsvall/teamssender/api/TeamsController.java
+++ src/main/java/se/sundsvall/teamssender/api/TeamsController.java
@@ -29,47 +29,47 @@ public class TeamsController {
 
 	@PostMapping("{municipalityId}/teams/messages")
 	@Operation(summary = "Send a message in Microsoft Teams", responses = {
-			@ApiResponse(
-					responseCode = "200",
-					description = "Message sent successfully",
-					useReturnTypeSchema = true),
+		@ApiResponse(
+			responseCode = "200",
+			description = "Message sent successfully",
+			useReturnTypeSchema = true),
 
-			@ApiResponse(
-					responseCode = "400",
-					description = "Invalid request payload or parameters",
-					content = @Content(schema = @Schema(oneOf = {
-							Problem.class,
-							ConstraintViolationProblem.class
-					}))),
+		@ApiResponse(
+			responseCode = "400",
+			description = "Invalid request payload or parameters",
+			content = @Content(schema = @Schema(oneOf = {
+				Problem.class,
+				ConstraintViolationProblem.class
+			}))),
 
-			@ApiResponse(
-					responseCode = "404",
-					description = "Chat could not be found or created",
-					content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(
+			responseCode = "404",
+			description = "Chat could not be found or created",
+			content = @Content(schema = @Schema(implementation = Problem.class))),
 
-			@ApiResponse(
-					responseCode = "422",
-					description = "Message could not be created or sent",
-					content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(
+			responseCode = "422",
+			description = "Message could not be created or sent",
+			content = @Content(schema = @Schema(implementation = Problem.class))),
 
-			@ApiResponse(
-					responseCode = "401",
-					description = "Authentication or authorization issue",
-					content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(
+			responseCode = "401",
+			description = "Authentication or authorization issue",
+			content = @Content(schema = @Schema(implementation = Problem.class))),
 
-			@ApiResponse(
-					responseCode = "503",
-					description = "Connection issue to Microsoft Graph API",
-					content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(
+			responseCode = "503",
+			description = "Connection issue to Microsoft Graph API",
+			content = @Content(schema = @Schema(implementation = Problem.class))),
 
-			@ApiResponse(
-					responseCode = "500",
-					description = "Unexpected internal server error",
-					content = @Content(schema = @Schema(implementation = Problem.class)))
+		@ApiResponse(
+			responseCode = "500",
+			description = "Unexpected internal server error",
+			content = @Content(schema = @Schema(implementation = Problem.class)))
 	})
 	public ResponseEntity<String> sendMessage(
-			@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @ValidMunicipalityId @PathVariable final String municipalityId,
-			@Valid @RequestBody final SendTeamsMessageRequest request) {
+		@Parameter(name = "municipalityId", description = "Municipality ID", example = "2281") @ValidMunicipalityId @PathVariable final String municipalityId,
+		@Valid @RequestBody final SendTeamsMessageRequest request) {
 
 		try {
 			// Plocka ut token från header, ta bort "Bearer "
