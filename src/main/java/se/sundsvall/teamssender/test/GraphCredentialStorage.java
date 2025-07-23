a33cc8280b85158d4233ce30cbe63d2388cbd34a
mar19vik <maria.wiklund@sundsvall.se>
2025-07-23 10:35:09 +0200

    WIP

--- src/main/java/se/sundsvall/teamssender/test/GraphCredentialStorage.java
+++ src/main/java/se/sundsvall/teamssender/test/GraphCredentialStorage.java
@@ -1,21 +1,21 @@
-package se.sundsvall.teamssender.test;
-
-import com.azure.identity.AuthorizationCodeCredential;
-import org.springframework.stereotype.Component;
-
-@Component
-public class GraphCredentialStorage {
-	private AuthorizationCodeCredential credential;
-
-	public synchronized void setCredential(AuthorizationCodeCredential credential) {
-		this.credential = credential;
-	}
-
-	public synchronized AuthorizationCodeCredential getCredential() {
-		return credential;
-	}
-
-	public boolean isAuthenticated() {
-		return credential != null;
-	}
-}
+//package se.sundsvall.teamssender.test;
+//
+//import com.azure.identity.AuthorizationCodeCredential;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class GraphCredentialStorage {
+//	private AuthorizationCodeCredential credential;
+//
+//	public synchronized void setCredential(AuthorizationCodeCredential credential) {
+//		this.credential = credential;
+//	}
+//
+//	public synchronized AuthorizationCodeCredential getCredential() {
+//		return credential;
+//	}
+//
+//	public boolean isAuthenticated() {
+//		return credential != null;
+//	}
+//}
