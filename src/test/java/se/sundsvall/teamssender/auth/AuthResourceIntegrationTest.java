a10905b1de5b724d6a30b7302e49c3d830804fcd
Per-Erik Brorsson <per-erik.brorsson@sundsvall.se>
2025-08-18 09:30:43 +0200

    Municipality ID test fixat TokenServiceTest.java

--- src/test/java/se/sundsvall/teamssender/auth/AuthResourceIntegrationTest.java
+++ src/test/java/se/sundsvall/teamssender/auth/AuthResourceIntegrationTest.java
@@ -1,5 +1,10 @@
 package se.sundsvall.teamssender.auth;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,67 +15,62 @@ import org.springframework.test.web.servlet.MockMvc;
 import se.sundsvall.teamssender.auth.service.TokenService;
 import se.sundsvall.teamssender.configuration.AzureConfig;
 
-import java.util.Map;
-
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @WebMvcTest(AuthResource.class)
 class AuthResourceIntegrationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Mock
-    private TokenService tokenService;
+	@Mock
+	private TokenService tokenService;
 
-    @Mock
-    private AzureConfig azureConfig;
+	@Mock
+	private AzureConfig azureConfig;
 
-    @TestConfiguration
-    static class MockConfig {
-        @Bean
-        TokenService tokenService(@Mock TokenService tokenService) {
-            return tokenService;
-        }
+	@TestConfiguration
+	static class MockConfig {
+		@Bean
+		TokenService tokenService(@Mock TokenService tokenService) {
+			return tokenService;
+		}
 
-        @Bean
-        AzureConfig azureConfig(@Mock AzureConfig azureConfig) {
-            return azureConfig;
-        }
-    }
-    @Test
-    void login_validMunicipality_returnsRedirect() throws Exception {
-        var mockAzure = mock(AzureConfig.Azure.class);
-        when(mockAzure.getLoginUrl()).thenReturn("https://login.microsoftonline.com/test");
+		@Bean
+		AzureConfig azureConfig(@Mock AzureConfig azureConfig) {
+			return azureConfig;
+		}
+	}
 
-        var adMap = Map.of("2281", mockAzure);
-        when(azureConfig.getAd()).thenReturn(adMap);
+	@Test
+	void login_validMunicipality_returnsRedirect() throws Exception {
+		var mockAzure = mock(AzureConfig.Azure.class);
+		when(mockAzure.getLoginUrl()).thenReturn("https://login.microsoftonline.com/test");
 
-        mockMvc.perform(get("/2281/login"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("https://login.microsoftonline.com/test"));
-    }
+		var adMap = Map.of("2281", mockAzure);
+		when(azureConfig.getAd()).thenReturn(adMap);
 
-    @Test
-    void login_invalidMunicipality_returns404() throws Exception {
-        // Tom map, dvs ingen kommun är giltig
-        when(azureConfig.getAd()).thenReturn(Map.of());
+		mockMvc.perform(get("/2281/login"))
+			.andExpect(status().is3xxRedirection())
+			.andExpect(redirectedUrl("https://login.microsoftonline.com/test"));
+	}
 
-        mockMvc.perform(get("/9999/login"))
-                .andExpect(status().isNotFound());
-    }
+	@Test
+	void login_invalidMunicipality_returns404() throws Exception {
+		// Tom map, dvs ingen kommun är giltig
+		when(azureConfig.getAd()).thenReturn(Map.of());
 
-    @Test
-    void callback_returnsExpectedResponseFromTokenService() throws Exception {
-        when(tokenService.exchangeAuthCodeForToken("abc123", "2281"))
-                .thenReturn(org.springframework.http.ResponseEntity.ok("token-data"));
+		mockMvc.perform(get("/9999/login"))
+			.andExpect(status().isNotFound());
+	}
 
-        mockMvc.perform(get("/callback")
-                        .param("code", "abc123")
-                        .param("state", "2281"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("token-data"));
-    }
+	@Test
+	void callback_returnsExpectedResponseFromTokenService() throws Exception {
+		when(tokenService.exchangeAuthCodeForToken("abc123", "2281"))
+			.thenReturn(org.springframework.http.ResponseEntity.ok("token-data"));
+
+		mockMvc.perform(get("/callback")
+			.param("code", "abc123")
+			.param("state", "2281"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("token-data"));
+	}
 }
